a2c07a5e4490cd242b2ed33b25239517ba71389b
Shoniwa Marovatsanga <stmarovatsanga@gmail.com>
2017-02-26 23:05:39 +0000

    Create Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  has_attached_file :image, styles: { medium: "600x600>"
+  has_attached_file :image, styles: { medium: "600x600>"}
   validates :image, presence: true
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170226211819) do
+ActiveRecord::Schema.define(version: 20170226213051) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "caption"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170226211819) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "comment"
   end
 
 end
