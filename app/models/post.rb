f67fd333bd627d4f8202774ecd3c8ba4c34a80e8
Shoniwa Marovatsanga <stmarovatsanga@gmail.com>
2017-02-26 21:21:38 +0000

    Added posts model

---
+++ app/models/post.rb
@@ -0,0 +1,5 @@
+class Post < ApplicationRecord
+  has_attached_file :image, styles: { medium: "600x600>"
+  validates :image, presence: true
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+end
